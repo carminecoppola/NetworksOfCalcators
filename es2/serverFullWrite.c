6de3046b2ab0fb1014f8271a82e60c2a8b00a712
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-10-27 09:37:36 +0200

    c

--- es2/serverFullWrite.c
+++ es2/serverFullWrite.c
@@ -12,42 +12,37 @@ ssize_t FullWrite(int fd, const void *buf, size_t count);
 
 int main(int argc, char **argv)
 {
-  int          listenfd, connfd;
-  struct sockaddr_in  servaddr;
-  char        buff[4096];
-  time_t        ticks;  
-
-  if ( ( listenfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0 ) {
-    perror("socket");
-    exit(1);
+    int          listenfd, connfd;
+    struct sockaddr_in  servaddr;
+    char        buff[4096];
+    time_t        ticks;  
+    if ( ( listenfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0 ) {
+        perror("socket");
+        exit(1);
+        }
+    servaddr.sin_family      = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port        = htons(1024);
+    if ( bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0 ) {
+        perror("bind");
+        exit(1);
     }
-
-  servaddr.sin_family      = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port        = htons(1024);
-
-  if ( bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0 ) {
-    perror("bind");
-    exit(1);
-  }
-
-  if ( listen(listenfd, 1024) < 0 ) {
-    perror("listen");
-    exit(1);
-  }
-  
-  for ( ; ; ) {
-    if ( ( connfd = accept(listenfd, (struct sockaddr *) NULL, NULL) ) < 0 ) {
-      perror("accept");
-      exit(1);
+    if ( listen(listenfd, 1024) < 0 ) {
+        perror("listen");
+        exit(1);
     }
-    ticks = time(NULL);
-    snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+    for ( ; ; ) {
+        if ( ( connfd = accept(listenfd, (struct sockaddr *) NULL, NULL) ) < 0 ) {
+            perror("accept");
+            exit(1);
+        }
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
 
-	  FullWrite(listenfd, buff, connfd);
+        FullWrite(connfd, buff, sizeof(buff));
 
-    close(connfd);
-  } 
+        close(connfd);
+    }
 }
 
 ssize_t FullWrite(int fd, const void *buf, size_t count)  
@@ -56,16 +51,18 @@ ssize_t FullWrite(int fd, const void *buf, size_t count)
     ssize_t nwritten; 
     nleft = count; 
     while (nleft > 0) {             /* repeat until no left */ 
-      write(fd,buf,nleft);
-
-      if (errno == EINTR) /* if interrupted by sysem call*/
-        continue;
-      else
-        exit(1);
-
-        nleft = read(fd, &buf, nleft);
+        if((nwritten = write(fd, buf, nleft))<0){
+            if(errno == EINTR){
+                continue;
+            }
+            else{
+                exit(EXIT_FAILURE);
+            }
+        }
+        else{
+            nleft -= nwritten;
+            buf += nwritten;
+        }    
     } 
-    buf = 0;
     return (nleft); 
 }
-
